8d1f352b595948168817e4af974ea8531248d605
laloparkour <laloparkour09@gmail.com>
2020-05-22 12:41:11 -0600

    git commit -m "Curso Javscript"

---
+++ app.js
@@ -0,0 +1,13 @@
+let typeCard = 'Credit Card';
+
+switch (typeCard) {
+    case 'Credit Card':
+        document.write('This is a credit card.');
+        break;
+    case 'Debit Card':
+        document.write('This is a debit card.');
+        break;
+    default:
+        document.write('No card.');
+        break;
+}
